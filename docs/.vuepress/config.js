70dd738c856d5550be4e7af63747da2b357b466a
搋兔 <naruto6404@hotmail.com>
2023-07-17 13:30:06 +0800

    upgrade: to vuepress `2.0.0-beta.66`

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,81 +1,77 @@
-const { path } = require('@vuepress/utils')
-const vuepress_plugin_simple_blog = require('../../lib/index.js')
+import { defineUserConfig } from "vuepress";
+import { defaultTheme } from "@vuepress/theme-default";
+import { getDirname, path } from "@vuepress/utils";
+import { registerComponentsPlugin } from "@vuepress/plugin-register-components";
 
-module.exports = {
-  title: 'vuepress-plugin-simple-blog',
-  description: 'A simple blog plugin for vuepress v2.',
+const __dirname = getDirname(import.meta.url);
+import vuepress_plugin_simple_blog from "../../lib/index.js";
+
+export default defineUserConfig({
+  title: "vuepress-plugin-simple-blog",
+  description: "A simple blog plugin for vuepress v2.",
   locales: {
-    '/': {
-      lang: 'en-US',
-      description: 'A simple blog plugin for Vuepress v2',
+    "/": {
+      lang: "en-US",
+      description: "A simple blog plugin for Vuepress v2",
     },
-    // '/zh/': {
-    //   lang: 'zh-TW',
-    //   description: '專為 Vuepress v2 而生的簡易部落格插件',
+    // "/zh/": {
+    //   lang: "zh-TW",
+    //   description: "專為 Vuepress v2 而生的簡易部落格插件",
     // },
   },
-  themeConfig: {
-    repo: 'https://github.com/huibizhang/vuepress-plugin-simple-blog',
-    docsRepo: 'https://github.com/huibizhang/vuepress-plugin-simple-blog',
-    docsBranch: 'master',
-    docsDir: 'docs',
+  theme: defaultTheme({
+    repo: "https://github.com/huibizhang/vuepress-plugin-simple-blog",
     contributors: false,
     locales: {
-      '/': {
-        selectLanguageName: 'English',
+      "/": {
+        selectLanguageName: "English",
       },
-      '/zh/': {
-        selectLanguageName: '繁體中文',
-        selectLanguageText: '切換語言',
-        selectLanguageAriaLabel: '切換語言',
+      "/zh/": {
+        selectLanguageName: "繁體中文",
+        selectLanguageText: "切換語言",
+        selectLanguageAriaLabel: "切換語言",
       },
     },
     navbar: [
       {
-        text: 'Guide',
-        link: '/guide/'
+        text: "Guide",
+        link: "/guide/",
       },
       {
-        text: 'Customization',
-        link: '/customization/'
+        text: "Customization",
+        link: "/customization/",
       },
       {
-        text: 'APIs',
-        link: '/api/'
+        text: "APIs",
+        link: "/api/",
       },
     ],
     sidebar: {
-      '/guide/': [
+      "/guide/": [
         {
-          text: 'Guide',
-          children: [
-            '/guide/',
-            '/guide/getting-started/',
-          ]
+          text: "Guide",
+          children: ["/guide/index.md", "/guide/getting-started.md"],
         },
       ],
-      '/customization/': [
+      "/customization/": [
         {
-          text: 'Customization',
-          link: '/customization/',
+          text: "Customization",
+          link: "/customization/",
         },
       ],
-      '/api/': [
+      "/api/": [
         {
-          text: 'Client APIs',
-          link: '/api/',
-          children: [
-            '/api/pagination/',
-            '/api/tag-list/',
-          ]
+          text: "Client APIs",
+          link: "/api/",
+          children: ["/api/pagination.md", "/api/tag-list.md"],
         },
       ],
     },
-  },
+  }),
   plugins: [
-    vuepress_plugin_simple_blog,
-    ['@vuepress/register-components', {
-      componentsDir: path.resolve(__dirname, './global_components'),
-    }],
-  ]
-}
+    vuepress_plugin_simple_blog(),
+    registerComponentsPlugin({
+      componentsDir: path.resolve(__dirname, "./global_components"),
+    }),
+  ],
+});
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./lib/**/*.{js,vue}"
+  ],
+  darkMode: 'class',
+  corePlugins: {
+    preflight: false,
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
